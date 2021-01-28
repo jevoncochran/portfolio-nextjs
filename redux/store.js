62f76214c958548f019838c66b232972b54ba1a0
Jevon Cochran <jevon.cochran@gmail.com>
2021-01-28 02:52:47 -0800

    removed redux persist

--- redux/store.js
+++ redux/store.js
@@ -2,17 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
-import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 
-// set up to persist redux state on refresh
-const persistConfig = {
-  key: "root",
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const store = createStore(persistedReducer, applyMiddleware(thunk, logger));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 export default store;
